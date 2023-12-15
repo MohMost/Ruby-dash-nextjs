deb4a35a15cf2425e3b8325312b4e7b89241814a
MohMost <teemohmost2020@gmail.com>
2023-12-15 07:31:24 +0100

    first commit

---
+++ app/blogs/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from '../page.module.css'
+
+export default function blogs() {
+  return (
+    <main className={styles.main}>
+      <h2>hello 2</h2>
+    </main>
+  )
+}
